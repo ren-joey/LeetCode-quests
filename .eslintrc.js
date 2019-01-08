ddad407139ff9fae288aeca4d5efbca56031cad9
Su-Chen Chang <su-chenchang@Su-Chende-MacBook-Pro.local>
2019-01-08 11:23:55 +0800

    刪除用不到的依賴，新增readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,10 +19,7 @@ module.exports = {
             "error",
             2
         ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
+        "linebreak-style": "off",
         "quotes": [
             "error",
             "single"
